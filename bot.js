ac61cf59f7d4ec2f6b3627a3dd398cfcbb95b463
Arturs Sosins <ar2rsawseen@gmail.com>
2017-07-07 09:28:39 +0300

    Dynamically load conversations based on config

--- bot.js
+++ bot.js
@@ -9,11 +9,17 @@ var bot = controller.spawn({
     token: config.slack_token
 }).startRTM();
 var helps = [];
-require("./conversations/talk")(controller, bot, helps);
-require("./conversations/apps")(controller, bot, helps);
-require("./conversations/management")(controller, bot, helps);
 
-controller.hears(['help me', 'what(.*)can(.*)do', 'need(.*)help'], 'direct_message,direct_mention,mention', function(bot, message) {
+if(config.conversations){
+    for(var i in config.conversations){
+        try{
+            require("./conversations/"+i)(controller, bot, helps, config.conversations[i]);
+        }
+        catch(ex){console.log(ex);}
+    }
+}
+
+controller.hears(['help me', 'what([^!.?,]*)can([^!.?,]*)do', 'need([^!.?,]*)help'], ['ambient','direct_message','direct_mention', 'mention'], function(bot, message) {
     var resp = "You can say:\n";
     for(var i = 0; i < helps.length; i++){
         resp += helps[i]+"\n";
